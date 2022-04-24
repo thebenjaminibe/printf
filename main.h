2a5c4cd8494d1cf8348bf385c744490e1cee2e6e
thebenjaminibe <frankynoble@gmail.com>
2022-04-24 05:08:26 -0700

    comment

--- main.h
+++ main.h
@@ -1,32 +1,23 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ * struct op - Struct op
+ *
+ * @arg: The argument
+ * @f: The function associated
  */
-typedef struct print
+typedef struct op
 {
-	char *t;
-	int (*f)(va_list);
+	char *arg;
+	void (*f)(va_list arg, int *count);
 } print_t;
 
+void (*get_func(const char *s, int index))(va_list, int *count);
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
-
-#endif  /* _MAIN_H */
+char *convert(unsigned int, int);
+void print_string(va_list arg, int *count);
+void print_c(va_list arg, int *count);
+void print_prg(int *count);
+#endif /* MAIN_H */
